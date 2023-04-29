059939d54857e778bd9dccf42f3f661e3e79045a
minjun0707 <jmk7117@naver.com>
2023-04-30 01:44:17 +0900

    feat : URL 등록 폼 구현

--- src/components/card.vue
+++ src/components/card.vue
@@ -1,31 +1,40 @@
 <template>
-    <div class="card shadow-sm shadow p-3 mb-5 bg-body rounded" v-on:mouseover = "mouseover" v-on:mouseleave = "mouseleave" @click="selectCard">
-          <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
-            <title>Placeholder</title>
-            <rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+  <div class="card shadow-sm shadow p-3 mb-5 bg-body rounded" v-on:mouseover="mouseover" v-on:mouseleave="mouseleave"
+    @click="selectCard">
+    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img"
+      aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
+      <title>Placeholder</title>
+      <rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
+    </svg>
 
-          <div class="card-body">
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            <div class="d-flex justify-content-between align-items-center">
-              <div class="btn-group">
-                <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-              </div>
-              <small class="text-muted">9 mins</small>
-            </div>
-          </div>
+    <div class="card-body">
+      <h5 class="card-text">content is a little bit longer.</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.
+        This content is a little bit longer.</p>
+      <div class="d-flex justify-content-between align-items-center">
+        <div class="btn-group">
+          <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+          <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
         </div>
- </template>
+        <small class="text-muted">김민준</small>
+      </div>
+    </div>
+
+    <div class="card-footer">
+      <small class="text-muted float-end">2023년도 8월 9일</small>
+    </div>
+  </div>
+</template>
    
-   <script>
-   export default {
-     name: 'card',
-     data() {
-      return {
-        isSelected : false,
-      }
-     },
-     
+<script>
+export default {
+  name: 'card',
+  data() {
+    return {
+      isSelected: false,
+    }
+  },
+
 
   methods: {
     mouseover() {
@@ -40,18 +49,16 @@
     selectCard() {
       console.log("click");
     },
-  },  
-   }
-   </script>
+  },
+}
+</script>
 
-<style>
-</style>
+<style></style>
 
 
    
    <!-- Add "scoped" attribute to limit CSS to this component only -->
-   <style scoped>
-   </style>
+<style scoped></style>
 
 
    
--- src/pages/mainHome.vue
+++ src/pages/mainHome.vue
@@ -1,46 +1,105 @@
 <template>
-    <main>
+  <main>
+    <section class="py-5 text-center container">
+      <div class="row py-lg-2">
+        <div class="col-lg-6 col-md-8 mx-auto">
 
-<section class="py-5 text-center container">
-  <div class="row py-lg-2">
-    <div class="col-lg-6 col-md-8 mx-auto">
-      <h3> 등록하고싶은 글에 URL을 입력해주세요</h3>
-      <input type="email mt-10" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
-      <button type="button mt-0" class="btn btn-success">등록</button>
 
-    </div>
-  </div>
-</section>
+          <div class="mb-3">
+            <h3> 등록하고싶은 글에 URL을 입력해주세요</h3>
+          </div>
 
-<div class="album py-5 bg-light">
-  <div class="container">
 
-    <div class="row row-cols-1 -2 row-cols-md-3 g-3">
-      <div class="col" v-for = "i in 4" :key="i">
+          <div class="row mt-3">
+            <div class="col-md-10">
+              <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+            </div>
+            <div class="col-md-2">
+              <button type="button" class="btn btn-success w-100" data-bs-toggle="modal"
+                data-bs-target="#create">등록</button>
 
-        <card/>
+              <!-- Modal -->
+              <div class="modal fade" id="create" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-lg">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
 
+                    <!-- body -->
+                    <div class="modal-body modal-lg" >
+                      <div class="card mb-3" style="max-width: 800px;">
+                        <div class="row g-0">
+                          <div class="col-md-5">
+                            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img"
+      aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
+      <title>Placeholder</title>
+      <rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
+    </svg>
+                          </div>
+                          <div class="col-md-7">
+                            <div class="card-body">
+                              <h5 class="card-title">제목은 파싱해서</h5>
+                              <p class="card-text"></p>
+                              <p class="card-text"><small class="text-muted">디스크립션</small></p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+
+
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                      <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+        </div>
       </div>
-     
-    </div>
-  </div>
-</div>
 
-</main>
+
+    </section>
+
+
+
+    <div class="album py-5 bg-light">
+      <div class="container">
+
+        <div class="row row-cols-1 -2 row-cols-md-3 g-3">
+          <div class="col" v-for="i in 4" :key="i">
+
+            <card />
+
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+
+
+  </main>
 </template>
 
-   <script>
-   import card from '@/components/card'
-   export default {
-     name: 'mainHome',
-     components: {
-      card
+<script>
+import card from '@/components/card'
+export default {
+  name: 'mainHome',
+  components: {
+    card
   }
-     
-   }
-   </script>
+
+
+}
+</script>
    
    <!-- Add "scoped" attribute to limit CSS to this component only -->
-   <style scoped>
-   </style>
+<style scoped></style>
    
